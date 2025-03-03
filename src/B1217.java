2fe5d490ab09763587046573967181da34978347
Dongun614 <72856107+Dongun614@users.noreply.github.com>
2025-03-03 10:54:11 +0900

    Initial commit

---
+++ src/B1217.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class B1217{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int asset;
+        int member;
+
+        asset = input.nextInt();
+        member = input.nextInt();
+
+        //System.out.println("asset = " + asset + "\n" + "member = " + member + "\n");
+    }
+}
