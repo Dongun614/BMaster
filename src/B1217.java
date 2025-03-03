9877c6dbb0191f30d636e88026d671edfb58acf0
Dongun614 <72856107+Dongun614@users.noreply.github.com>
2025-03-03 11:24:29 +0900

    update: B1217

--- src/B1217.java
+++ src/B1217.java
@@ -3,14 +3,23 @@ import java.util.Scanner;
 public class B1217{
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-        int asset;
-        int member;
+        int asset; //최백준 조교가 가진 돈 n
+        int member; //나눠줘야할 인원 수 m
+        //int rest; //나눠주고 남은 값
 
         asset = input.nextInt();
         member = input.nextInt();
 
+//        if(asset % member < 1){ //만약 n/m 후 남은 값이 1 미만이라면 해당 값을 출력, 아니면 0을 출력
+//            rest = asset % member;
+//        }
+//        else{
+//            rest = 0;
+//        }
+
+        //테스트 코드
         //System.out.println("asset = " + asset + "\n" + "member = " + member + "\n");
-
-
+        System.out.println(asset/member + "\n" + asset%member);
+        System.exit(0);
     }
 }
