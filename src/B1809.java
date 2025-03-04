72e6fa33a270193ab8c7ea006f32b46d4c770238
Dongun614 <72856107+Dongun614@users.noreply.github.com>
2025-03-04 19:48:08 +0900

    create: B1809

---
+++ src/B1809.java
@@ -0,0 +1,10 @@
+public class B1809 {
+    public static void main(String[] args){
+        System.out.println("(___)\n");
+        System.out.println("(o o)____/\n");
+        System.out.println(" @@      \\\n");
+        System.out.println("  \\ ____,/\n");
+        System.out.println("  //   //\n");
+        System.out.println(" ^^   ^^\n");
+    }
+}
